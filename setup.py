33491f68eec224d70d3f7426e1854acaf8fc555b
Doug Feldmann <douglas_feldmann@hms.harvard.edu>
2016-08-03 08:10:19 -0400

    Packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='slackbotmaker',
-            version='0.1',
+            version='0.0.1',
             description='Trivial framework for building a slackbot rtm client',
             url='http://github.com/dmf24/slackbotmaker',
             author='Doug Feldmann',
             author_email='doug@feldmann.com',
             license='MIT',
             packages=['slackbotmaker'],
+            install_requires=[
+                    'slackclient',
+                ],
             zip_safe=False)
